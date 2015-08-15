9f06803c5294244167dfb21b19961d6c0e59b919
M.Arslan <marslanc@users.noreply.github.com>
2015-08-15 19:19:15 +0500

    v1.0.1

---
+++ admin/ui/help.php
@@ -0,0 +1,15 @@
+<div class="sip-rswc-content">
+  <div style="display: block;">
+    <h2>Usage</h2>
+    <p><strong class="strong">Shortcode usage example:</strong> [woocommerce_reviews id="2075" product_title="My Awesome Product" no_of_reviews="5" ]</p>
+    <p><strong>id</strong> (optional) is the ID of the WooCoommerce product you want to display. [woocommerce_reviews] without id input will show the reviews of the current product if applicable.<br>
+      <strong>product_title</strong> (optional) is the name of the product you want to display. This is used for Schema only (by default is the product name in WooCommerce).<br>
+      <strong>no_of_reviews</strong> (optional) is the number of reviews loaded per page (default = 5). Additional reviews can be loaded after clicking in "Load more" button.<br>
+    </p>
+    <p>SIP Reviews Shortcode for WooCommerce is fully-compatible with Schema.org, allowing you to display product ratings directly from Google's results. You may test your Schema at <a href="https://developers.google.com/structured-data/testing-tool/">Google Structured Data Testing Tool</a>.</p>
+    <h2>Questions and support</h2>
+    <p>All of our plugins come with free support. We care about your plugin after purchase just as much as you do.</p>
+    <p>We want to make your life easier and make you happy about choosing our plugins. We guarantee to respond to every inquiry within 1 business day.
+    Please visit our <a href="https://shopitpress.com/community/?utm_source=wordpress.org&amp;utm_medium=backend-help&amp;utm_campaign=sip-reviews-shortcode-woocommerce" target="_blank">community</a> and ask us anything you need.</p>
+  </div>
+</div>
