da9cf2d95f69a938fa73b35bc86516e6cc43976e
ShopitPress <hello@shopitpress.com>
2015-09-09 18:00:38 +0800

    v 1.0.2

--- sip-reviews-shortcode-wc.php
+++ sip-reviews-shortcode-wc.php
@@ -10,7 +10,7 @@
  * Plugin Name:       SIP Reviews Shortcode for WooCommerce
  * Plugin URI:        https://shopitpress.com/plugins/sip-reviews-shortcode-woocommerce/
  * Description: 	  	Creates a shortcode, [woocommerce_reviews id="n"],  that displays the reviews, of any WooCommerce product. [woocommerce_reviews] will show the reviews of the current product if applicable.  This plugin requires WooCommerce.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires:		  		PHP5, WooCommerce Plugin
  * Author:            ShopitPress <hello@shopitpress.com>
  * Author URI:        https://shopitpress.com
@@ -26,9 +26,9 @@ class SIP_Reviews_Shortcode_WC{
 
 	/**
 	 * A constructor, to create objects from a class.
-	 *		 		
+	 *
 	 * @since    1.0.0
-	 * @access   public		 
+	 * @access   public
 	 */
 	public function __construct() {
 		add_action( 'plugins_loaded', array( &$this, 'constants' ), 	1 );
@@ -39,13 +39,13 @@ class SIP_Reviews_Shortcode_WC{
 		add_action( 'wp_print_style', array( &$this, 'reviews_styles' ));
 		register_deactivation_hook( __FILE__, array( 'SIP_Reviews_Shortcode_WC_Admin' , 'sip_rswc_deactivate' ) );
 	}
-		
+
 	/**
 	 * Define a constant to use it anyware in the plugin.
-	 *		 		
+	 *
 	 * @since    1.0.0
-	 * @access   public		 
-	 */ 
+	 * @access   public
+	 */
 	public function constants() {
 		define( 'SIP_RSWC_NAME', 'SIP Reviews Shortcode for WooCommerce' );
 		define( 'SIP_RSWC_VERSION', '1.0.1' );
@@ -57,25 +57,25 @@ class SIP_Reviews_Shortcode_WC{
 		define( 'SIP_RSWC_PUBLIC', SIP_RSWC_DIR . trailingslashit( 'public' ) );
 		define( 'SIP_RSWC_PLUGIN_PURCHASE_URL', 'https://shopitpress.com/plugins/sip-reviews-shortcode-woocommerce/' );
 	}
-	
+
 	/**
-	 * A method to include the some file 
-	 *		 		
+	 * A method to include the some file
+	 *
 	 * @since    1.0.0
-	 * @access   public		 
-	 */ 
+	 * @access   public
+	 */
 	public function includes() {
 		require_once( SIP_RSWC_DIR .    'admin/sip-reviews-shortcode-admin.php' );
 		if( class_exists( 'SIP_Reviews_Shortcode_WC_Pro' ) ){ return; }
 			require_once( SIP_RSWC_PUBLIC . 'template-function.php' );
 	}
-		
+
 
 	/**
-	 * A method to load the css and javascript files 
-	 *		 		
+	 * A method to load the css and javascript files
+	 *
 	 * @since    1.0.0
-	 * @access   public		 
+	 * @access   public
 	 */
 	public function reviews_register() {
 		if( !is_admin() ) {
@@ -87,40 +87,40 @@ class SIP_Reviews_Shortcode_WC{
       		wp_enqueue_script( 'jquery' );
 		}
 	}
-      
+
 	/**
 	 * To chek the woocommerce is active or not
-	 *		 		
+	 *
 	 * @since    1.0.0
-	 * @access   public		 
+	 * @access   public
 	 */
 	public function activate () {
-		$plugin = plugin_basename( __FILE__ );		
+		$plugin = plugin_basename( __FILE__ );
 		if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 			if( is_plugin_active( $plugin ) ) {
 				deactivate_plugins( $plugin );
 			}
 		}
 	}
-		
+
 	/**
 	 * Give the error for activate the woocommerce plugin
-	 *		 
+	 *
 	 *
 	 * @since    1.0.0
 	 * @access   public
 	 */
 	public function plugin_notice_message () {
 		if ( !is_plugin_active ( 'woocommerce/woocommerce.php' ) ) {
-			 include_once( SIP_RSWC_PUBLIC . 'reviews-shortcode-error.php' ); 
-		} 
+			 include_once( SIP_RSWC_PUBLIC . 'reviews-shortcode-error.php' );
+		}
 	}
-		
+
 	/**
 	 * To check the plugin woocomerce product review pro is active or not
-	 *		 		
+	 *
 	 * @since    1.0.0
-	 * @access   public		 
+	 * @access   public
 	 * @return 	 bool True/False
 	 */
 	public function product_reviews_pro() {
